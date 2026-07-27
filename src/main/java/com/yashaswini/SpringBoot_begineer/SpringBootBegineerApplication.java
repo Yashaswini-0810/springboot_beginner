43fa5cc6b4cfb0ce0983e2cf30e027fce4c4bb19
Yashaswini-0810 <yashaswini0810@gmail.com>
2026-07-27 22:57:36 +0530

    Added spring files

---
+++ src/main/java/com/yashaswini/SpringBoot_begineer/SpringBootBegineerApplication.java
@@ -0,0 +1,13 @@
+package com.yashaswini.SpringBoot_begineer;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootBegineerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootBegineerApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/yashaswini/SpringBoot_begineer/SpringBootBegineerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.yashaswini.SpringBoot_begineer;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootBegineerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
